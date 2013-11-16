611d54db9ee5f10e9d058dddb196b8f21268f87c
chaoyee <chaoyee22@gmail.com>
2013-11-16 21:01:21 +0800

    bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :current_user, :logged_in?, :already_in_queue_item?
-
+  helper_method :current_user, :logged_in?
   def current_user
     User.find(session[:user_id]) if session[:user_id]    
   end
