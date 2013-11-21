581013d48cd2eedc566fada39ba1d97334d4c958
chaoyee <chaoyee22@gmail.com>
2013-11-21 15:41:33 +0800

    A more complicated feature spec

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -3,10 +3,7 @@ require 'spec_helper'
 feature "user sign in" do
   scenario "with valid email and password" do
     bob = Fabricate(:user)
-    visit sign_in_path
-    fill_in "Email", with: bob.email
-    fill_in "Password", with: bob.password
-    click_button "Sign in"
+    sign_in(bob)
     page.should have_content bob.full_name
   end
 end
