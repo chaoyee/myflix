4cb5059f2e142168b207ba3c1ac94d5ecb0ad7bb
chaoyee <chaoyee22@gmail.com>
2014-03-26 21:44:55 +0800

    carriewave modification and remove columns of videos

--- app/uploaders/large_cover_uploader.rb
+++ app/uploaders/large_cover_uploader.rb
@@ -1,5 +1,15 @@
 class LargeCoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  # stroe_dir: uploaded file directory
+  # default store_dir: /public/uploads
+  #
+  # Changing the storage directoty:
+  #
+  # def store_dir
+  #  'public/my/upload/directory'
+  # end
+
+
   process :resize_to_fill => [665, 375]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140110173455) do
+ActiveRecord::Schema.define(version: 20140326082753) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -76,8 +76,6 @@ ActiveRecord::Schema.define(version: 20140110173455) do
   create_table "videos", force: true do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "small_cover_url"
-    t.string   "large_cover_url"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
